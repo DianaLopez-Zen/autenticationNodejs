babaa90e9b33ec5784cc4db0e6a5f51c41d0bbc7
DianaLopez-Zen <1202.lopez.zendejas.diana@gmail.com>
2020-05-24 23:07:48 -0500

    Primer Commit

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3343098',
+        password: 'hbH1deHZ5y',
+        database: 'sql3343098'
+    }
+
+};
